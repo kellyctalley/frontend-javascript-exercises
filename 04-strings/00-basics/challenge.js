6d61f96d3e69f69a6f770ec9739e91ceabc9293a
Kelly Talley <kcutchen@gmail.com>
2015-03-22 21:45:24 +0000

    Finished sliceItAndCombineIt function

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -5,8 +5,12 @@ module.exports.formLetter = function(firstName, senderName, message) {
 module.exports.formLetter("Rob", "Joe", "Take care.")
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-
+  var string1 = bigString.substring(startA, endA);
+  var string2 = bigString.substring(startB, endB);
+  return string1 + string2
 };
+
+module.exports.sliceItAndCombineIt("This is a long test of the emergency broadcast system.", 10, 20, 30, 40);
 
 module.exports.findFirstMatch = function(text, searchString) {
 
