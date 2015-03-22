277997ec95a9f3ce763f2a4b681560f2cae5ab09
Kelly Talley <kcutchen@gmail.com>
2015-03-22 22:19:23 +0000

    Finished equalStrings function

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,6 +1,10 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+  var checkEqual = stringOne == stringTwo;
+  return checkEqual;
 };
+
+module.exports.equalStrings("fun", "fun");
+module.exports.equalStrings("bloc", "boring")
 
 module.exports.notEqual = function(one, two) {
 
