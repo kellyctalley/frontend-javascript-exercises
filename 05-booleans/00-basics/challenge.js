5e41554e478c482092eecc26bd758b5febb479bf
Kelly Talley <kcutchen@gmail.com>
2015-03-22 22:42:25 +0000

    Finished inBetween and outsideRanges functions

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -15,12 +15,22 @@ module.exports.notEqual(1, "1");
 module.exports.notEqual(1, 1);
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+  var middleNum = (lower < middle) && (middle < upper);
+  return middleNum;
 };
+module.exports.inBetween(1, 3, 5);
+module.exports.inBetween(3, 1, 5);
 
 module.exports.outsideRanges = function(number) {
-
+  var checkNum1 = (number < 10) || (number > 20);
+  var checkNum2 = (number <= 42) || (number > 75);
+  var checkNum3 = (number <= 1) || (number >= 6);
+  var numCheck = checkNum1 && checkNum2 && checkNum3;
+  return numCheck;
 };
+
+module.exports.outsideRanges(42);
+module.exports.outsideRanges(75);
 
 module.exports.nameAndPrice = function(name, price) {
 
