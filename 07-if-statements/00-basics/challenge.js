32d5711877919ed861788fc8643f64263def78d6
Kelly Talley <kcutchen@gmail.com>
2015-03-23 16:12:41 +0000

    Finished favoriteNumer function

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,4 +1,17 @@
-module.exports.favoriteNumber = undefined;
+//favorite number function
+module.exports.favoriteNumber = function(fav, guess) {
+  if (guess > fav) {
+    return "Too high";
+      } else if (guess < fav) {
+        return "Too low";
+      } else {
+        return "You got it!"
+      }
+};
+
+module.exports.favoriteNumber(10, 1);
+module.exports.favoriteNumber(5, 11);
+module.exports.favoriteNumber(11, 11);
 
 module.exports.checkLock = undefined;
 
