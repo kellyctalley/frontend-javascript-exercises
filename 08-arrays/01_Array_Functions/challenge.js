2f89622aa2a21d02b0f5619352ae212334fb01b0
Kelly Talley <kcutchen@gmail.com>
2015-03-23 22:00:23 +0000

    Finished plusesEverywhere function

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -8,6 +8,10 @@ module.exports.reversePlusOne = function(array) {
 
 module.exports.reversePlusOne([5, 4, 3, 2]);
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function(array) {
+  return array.join("+");
+};
+
+module.exports.plusesEverywhere([1, 2, 3, 4, 5]);
 
 module.exports.arrayQuantityPlusOne = undefined;
