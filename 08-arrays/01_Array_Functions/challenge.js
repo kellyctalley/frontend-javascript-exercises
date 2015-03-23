fa5eaa60d9d8cd89c7ce1b5e07e54ae23a82ffa4
Kelly Talley <kcutchen@gmail.com>
2015-03-23 21:46:57 +0000

    Finished reversePlusOne function

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,4 +1,12 @@
-module.exports.reversePlusOne = undefined;
+//reverse plus one function
+module.exports.reversePlusOne = function(array) {
+  array.push(1);
+  array;
+  array.reverse();
+  return array;
+};
+
+module.exports.reversePlusOne([5, 4, 3, 2]);
 
 module.exports.plusesEverywhere = undefined;
 
