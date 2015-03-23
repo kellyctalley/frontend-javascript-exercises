2c72bc0720e66d6c275b67ca1c5a979bb6f83eaa
Kelly Talley <kcutchen@gmail.com>
2015-03-23 16:01:21 +0000

    Finished Function exercises

--- 06-functions/00-basics/challenge.js
+++ 06-functions/00-basics/challenge.js
@@ -1,3 +1,14 @@
-module.exports.helloWorld = undefined;
+//hello world function
+module.exports.helloWorld = function() {
+  return "Hello World";
+};
 
-module.exports.multiply = undefined;
+module.exports.helloWorld();
+
+//multiply function
+module.exports.multiply = function(num1, num2) {
+  return num1 * num2;
+};
+
+module.exports.multiply(4, 7);
+module.exports.multiply(5, 12);
