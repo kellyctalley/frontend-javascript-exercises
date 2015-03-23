1fce2509ae3e53074c9359aaa15dbc0cc4bdd3fd
Kelly Talley <kcutchen@gmail.com>
2015-03-23 21:12:40 +0000

    Finished 00_Array_Definition section

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,19 @@
-module.exports.newArray = undefined;
+//new array function
+module.exports.newArray = function(one, two, three, four) {
+  var myArray = [one, two, three, four];
+  return myArray;
+};
 
-module.exports.firstAndLast = undefined;
+module.exports.newArray(1, 2, 3, 4);
+module.exports.newArray("John", "Paul", "George", "Ringo");
+
+//first and last function
+module.exports.firstAndLast = function(array) {
+  var first = array[0];
+  var lastElementIndex = array.length - 1;
+  var last = array[lastElementIndex];
+  return [first, last];
+};
+
+module.exports.firstAndLast([1, 2, 3]);
+module.exports.firstAndLast(["mango", "banana", "pineapple"]);
