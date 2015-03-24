0a127db498ee0b767cb6792309431fb031306483
Kelly Talley <kcutchen@gmail.com>
2015-03-24 01:02:40 +0000

    Finished addItem function

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,14 @@
-module.exports.addItem = undefined;
+//add item function
+module.exports.addItem = function(newItem, array) {
+    if (array.indexOf(newItem) > -1) {
+        return array;
+    } else {
+        array.push(newItem);
+        return array;
+    }
+};
+
+module.exports.addItem('orange', ['banana', 'apple', 'orange']);
+module.exports.addItem('orange', ['banana', 'apple', 'blueberries']);
 
 module.exports.reverseSortedList = undefined;
