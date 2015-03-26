4c3396dcf68281978b9a7afcd5781b1565146c72
Kelly Talley <kcutchen@gmail.com>
2015-03-26 21:17:47 +0000

    Finished sumNumbers function

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,4 +1,11 @@
-module.exports.sumNumbers = undefined;
+//sum numbers function
+module.exports.sumNumbers = function (array) {
+    var arrayTotal = 0;
+    for (var i = 0; i < array.length; i++) {
+        arrayTotal += array[i];
+    }
+    return arrayTotal
+};
 
 module.exports.splitAndLowerCaseString = undefined;
 
