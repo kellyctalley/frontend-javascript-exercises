580e998e76d2d67fa6f1bb3dd52bbaebd459153e
Kelly Talley <kcutchen@gmail.com>
2015-03-27 15:38:36 +0000

    Finished copy function

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,4 +1,11 @@
-module.exports.copy = undefined;
+//copy object function
+module.exports.copy = function (object) {
+    var secondObject = new Object ();
+    for (var property in object) {
+        secondObject[property] = object[property];
+    }
+    return secondObject
+};
 
 module.exports.extend = undefined;
 
