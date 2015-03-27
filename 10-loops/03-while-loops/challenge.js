bcf055c2031f7b14d2fa9ed4fee87284a45fe85e
Kelly Talley <kcutchen@gmail.com>
2015-03-27 19:55:39 +0000

    Finished loop exercises

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,7 +1,13 @@
-module.exports.stream = undefined;
+//stream function
+module.exports.stream = function(conditionalFn, actionFn) {
+    while(conditionalFn()) {
+        actionFn();
+    }
+};
+
 
 //sum numbers in array with while loop
-module.exports.sumNumbers = function (array) {
+module.exports.sumNumbers = function(array) {
     var total = 0;
     var i = 0;
     while(array[i]) {
