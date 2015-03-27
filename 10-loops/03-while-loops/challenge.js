793a0a619eff2ff5e0ac74e605d3de36deaa215f
Kelly Talley <kcutchen@gmail.com>
2015-03-27 19:39:56 +0000

    Finished sumNumbers function

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,13 @@
 module.exports.stream = undefined;
 
-module.exports.sumNumbers = undefined;
+//sum numbers in array with while loop
+module.exports.sumNumbers = function (array) {
+    var total = 0;
+    var i = 0;
+    while(array[i]) {
+        total += array[i];
+        i++;
+    }
+    return total;
+};
 
